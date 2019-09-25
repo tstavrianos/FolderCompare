fed8e504efab220248f6eb67f063b891ec7d2219
Theo <tstavrianos@users.noreply.github.com>
2019-09-25 10:04:40 +0100

    Actually works properly now and will report any files that existed during generation and did not exist during comparing.

--- FolderCompare/Program.cs
+++ FolderCompare/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
+using System.Threading.Tasks;
 using CommandLine;
 using Newtonsoft.Json;
 
@@ -11,80 +14,113 @@ namespace FolderCompare
     {
         private sealed class Options
         {
+            /* TODO: Figure out how to use this properly
             [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
             public bool Verbose { get; set; }
-            
+            */
+
             [Option('j', "json", Required = true, HelpText = "Json file to store to or read the hashes from")]
             public string Json { get; set; }
-            
+
             [Option('d', "directory", Required = true, HelpText = "Directory to check")]
             public string Folder { get; set; }
-            
-            [Option('g', "generate", Required = false, Default=false, HelpText = "Run in generator mode")]
+
+            [Option('g', "generate", Required = false, Default = false, HelpText = "Run in generator mode")]
             public bool Generate { get; set; }
         }
-        
-        private static readonly SHA256CryptoServiceProvider  Provider = new SHA256CryptoServiceProvider();
+
+        private static readonly SHA256CryptoServiceProvider Provider = new SHA256CryptoServiceProvider();
+
         public static void Main(string[] args)
         {
-
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(o =>
                 {
-                    var dic = !o.Generate ? JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(o.Json)) : new Dictionary<string, string>();
-            
-                    foreach(var file in Directory.GetFiles(o.Folder, "*", SearchOption.AllDirectories))
+                    if (!Directory.Exists(o.Folder))
                     {
-                        var hash = Hash(file);
-                        var relative = file.Remove(0, o.Folder.Length);
+                        Console.Error.WriteLine($"{o.Folder} does not exist");
+                        return;
+                    }
+
+                    if (!o.Generate && !File.Exists(o.Json))
+                    {
+                        Console.Error.WriteLine($"{o.Json} does not exist");
+                        return;
+                    }
+                    
+                    var dic = !o.Generate
+                        ? JsonConvert.DeserializeObject<ConcurrentDictionary<string, (string hash, long size)>>(File.ReadAllText(o.Json))
+                        : new ConcurrentDictionary<string, (string hash, long size)>();
+
+                    var issues = new ConcurrentDictionary<string, string>();
+                    
+                    var di = new DirectoryInfo(o.Folder);
+
+                    
+
+                    Parallel.ForEach(di.EnumerateFiles("*", SearchOption.AllDirectories), fi =>
+                    {
+                        var relative = fi.FullName.Remove(0, o.Folder.Length);
                         if (!o.Generate)
                         {
-                            var ok = true;
-                            if (!dic.ContainsKey(relative))
+                            var message = string.Empty;
+                            if (!dic.TryRemove(relative, out var source))
                             {
-                                ok = false;
-                            }
-                            else if(dic[relative] != hash)
-                            {
-                                ok = false;
+                                message = "Did not exist in the source directory";
                             }
 
-                            if (o.Verbose)
+                            if (fi.Length != source.size)
                             {
-                                Console.WriteLine("{0}, calculated Sha256 hash: {1}, matches: {2}", relative, hash, ok ? "yes" : "no");
+                                message = "Size does not match";
                             }
                             else
                             {
-                                if (!ok)
+                                var calculated = Hash(fi);
+
+                                if (source.hash != calculated.hash)
                                 {
-                                    Console.WriteLine("{0}, calculated Sha256 hash: {1}", relative, hash);
+                                    message = "Contents do not match";
                                 }
+                            }
+
+                            if (!string.IsNullOrEmpty(message))
+                            {
+                                issues.TryAdd(relative, message);
                             }
                         }
                         else
                         {
-                            dic.Add(relative, Hash(file));
-                            if (o.Verbose)
-                            {
-                                Console.WriteLine("{0}, calculated Sha256 hash: {1}", relative, hash);
-                            }
+                            dic.TryAdd(relative, Hash(fi));
+                        }
+                    });
+
+                    if (o.Generate)
+                    {
+                        foreach (var kv in dic)
+                        {
+                            issues.TryAdd(kv.Key, "Source file not found");
+                        }
+
+                        foreach (var kv in issues)
+                        {
+                            Console.WriteLine($"{kv.Key} => {kv.Value}");
                         }
                     }
-
-                    if (!o.Generate) return;
-                    var output = JsonConvert.SerializeObject(dic);
-                    File.WriteAllText(o.Json, output);
+                    else
+                    {
+                        var output = JsonConvert.SerializeObject(dic);
+                        File.WriteAllText(o.Json, output);
+                    }
                 });
-
         }
 
-        private static string Hash(string filename)
+        private static (string hash, long size) Hash(FileInfo fi)
         {
-            using (var f = File.OpenRead(filename))
+            using (var f = fi.OpenRead())
             using (var b = new BufferedStream(f, 8192))
             {
                 var hash = Provider.ComputeHash(b);
-                return BitConverter.ToString(hash).Replace("-", string.Empty);
+                return (BitConverter.ToString(hash).Replace("-", string.Empty), fi.Length);
             }
         }
     }
